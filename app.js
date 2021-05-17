3db63e66563d341dff8a7b3316ab514ed56ca0fc
SEKTION <s3kt10n21@gmail.com>
2021-05-18 00:09:30 +0530

    Digital/Analog Clock

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+window.onload = function() {
+  type.addEventListener("change", function () {
+    if (type.selectedIndex == 0) {
+      dTime.style.display = "none";
+      clock.style.display = "block";
+    } else if (type.selectedIndex == 1) {
+      clock.style.display = "none";
+      dTime.style.display = "block";
+    }
+  });
+}
+
 var sHand = document.querySelector("#second-hand");
 var mHand = document.querySelector("#minute-hand");
 var hHand = document.querySelector("#hour-hand");
-var dTime = document.querySelector(".time"); 
+var dTime = document.querySelector(".time");
+var clock = document.querySelector(".clock");
+var type = document.querySelector('#clk-type')
 
 function rotate() {
   let time = new Date();
@@ -14,7 +28,7 @@ function rotate() {
   sHand.style.transform = `rotate(${secDegrees}deg)`;
   mHand.style.transform = `rotate(${minDegrees}deg)`;
   hHand.style.transform = `rotate(${hourDegrees}deg)`;
-  dTime.innerHTML = `${hours > 12 ? hours - 12 : hours}:${mins > 9 ? mins: "0"+mins}:${secs > 9 ? secs : "0"+secs} ${hours >= 12 ? "PM" : "AM"}`;
+  dTime.innerHTML = `${hours > 12 ? hours - 12 : hours == 0 ? 12 : hours}:${mins > 9 ? mins: "0"+mins}:${secs > 9 ? secs : "0"+secs} ${hours >= 12 ? "PM" : "AM"}`;
 }
 
 rotate();
